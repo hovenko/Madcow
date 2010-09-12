8bbfe175b73a4e3be68da52478058b81cb56873b
Knut-Olav Hoven <knutolav@gmail.com>
2010-09-12 21:03:04 +0200

    Use the error_log while running tests

--- tests/setup_tests_environment.php
+++ tests/setup_tests_environment.php
@@ -3,9 +3,8 @@
 $dir = dirname(__FILE__);
 set_include_path("$dir/..:" . get_include_path());
 
-#define('LOG4PHP_CONFIGURATION', dirname(__FILE__).'/../logger.conf');
 require_once 'DF/Web/Logger.php';
-DF_Web_Logger::setActiveLogger('log4php');
+DF_Web_Logger::setActiveLogger('error_log');
 
 require_once 'simpletest/unit_tester.php';
 require_once 'simpletest/reporter.php';
